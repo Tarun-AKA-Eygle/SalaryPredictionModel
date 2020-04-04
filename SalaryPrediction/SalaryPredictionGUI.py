198cf88adbb4739fe0fc6f0aa41bf15a71dd3f1b
Tarun-AKA-Eygle <55961027+Tarun-AKA-Eygle@users.noreply.github.com>
2020-04-04 19:14:40 +0530

    Add files via upload

---
+++ SalaryPrediction/SalaryPredictionGUI.py
@@ -0,0 +1,139 @@
+import tkinter as tk
+from PIL import ImageTk,Image
+from SalaryPredictionModel import *
+
+insert = 0
+
+class g(tk.Tk):
+    def __init__(self,*args,**kwargs):
+        tk.Tk.__init__(self,*args,**kwargs)
+        self.title("SalaryPrediction")
+        self.iconbitmap('D:\Programswin\Ostl project\Proycontec-Robots-Robot-documents.ico')
+        
+        container = tk.Frame(self)
+        container.pack(side = 'top',fill='both',expand=True)
+        
+
+        container.grid_rowconfigure(0,weight=1)
+        container.grid_columnconfigure(0,weight=1)
+
+        self.frames ={}
+
+        for f in (Startpage,Pageone,Pagetwo):
+            frame = f(container,self)
+            self.frames[f] = frame
+
+            frame.grid(row=0,column=0,sticky='nsew')
+        self.show_frame(Startpage)
+
+    def show_frame(self,cont):
+        frame = self.frames[cont]
+        frame.tkraise()
+        
+
+class Startpage(tk.Frame):
+    def __init__(self,parent,controller):
+        tk.Frame.__init__(self,parent)
+        introduction='You work in the H.R. department of your company.\n While interviewing a person, you came to know that this salary of previous job was 30k.\nYou decided to fact check his clames.\nYou called his ex-company but due to some complicatiothey could only give you their dataset of one year.\nYour goal is to predict what his salary of previous job was....(Press Im in to proceed)'
+        label = tk.Label(self,text='Welcome! Mr.Homes')
+        labelquestion = tk.Label(self,text=introduction)
+        
+        iminbutton = tk.Button(self,text="I'm in",command=lambda: controller.show_frame(Pageone))
+       
+        label.grid(row=0,column=1)
+        labelquestion.grid(row=1,column=1)
+        iminbutton.grid(row=2,column=1)
+        
+        self.grid_rowconfigure(1,weight=1)
+        self.grid_columnconfigure(1,weight=1)
+
+class Pageone(tk.Frame):
+    
+      
+    def __init__(self,parent,controller):
+        tk.Frame.__init__(self,parent)
+        
+        label = tk.Label(self,text='what is the level?')
+        goback = tk.Button(self,text='go back',command=lambda: controller.show_frame(Startpage))
+        view = tk.Button(self,text='view dataset',command=self.showdata)
+        insert = tk.Entry(self,width=15)
+        submit = tk.Button(self,text='Submit',command=lambda: self.check(insert.get(),controller))
+        insert.insert(0,"Enter the level")
+        
+        
+        label.grid(row=0,column=1)
+        view.grid(row=1,column=1)
+        insert.grid(row=2,column=1)
+        goback.grid(row=3,column=1)
+        submit.grid(row=4,column=1)
+
+        self.grid_rowconfigure(1,weight=1)
+        self.grid_columnconfigure(1,weight=1)
+
+        
+      
+    def check(self,entry,controller):
+        global insert
+        try:
+            insert = float(entry)
+            controller.show_frame(Pagetwo)
+             
+        except:
+            tk.messagebox.showinfo("Error","INPUT MUST BE interger or float")
+                
+
+    def showdata(self):
+        root = tk.Tk()
+        root.title("SalaryPrediction")
+        root.iconbitmap('D:\Programswin\Ostl project\Proycontec-Robots-Robot-documents.ico')
+        
+        label = tk.Label(root,text=showdataset())
+        label.pack()
+        root.mainloop()
+        
+
+class Pagetwo(tk.Frame):
+    def __init__(self,parent,controller):
+
+       
+        
+        tk.Frame.__init__(self,parent)
+        backbutton = tk.Button(self,text='go back',command=lambda: controller.show_frame(Pageone))
+        Polybutton = tk.Button(self,text='Polynomial Linear Regression',command=self.showpolyreg)
+        Prediction = tk.Button(self,text='Prediction',command=self.prediction)
+        Linearbutton = tk.Button(self,text='Linear Regression',command=self.showlinearreg)
+        
+
+
+        backbutton.grid(row=0,column=1)
+        Polybutton.grid(row=1,column=1)
+        Linearbutton.grid(row=2,column=1)
+        Prediction.grid(row=3,column=1)
+
+        self.grid_rowconfigure(1,weight=1)
+        self.grid_columnconfigure(1,weight=1)
+
+
+
+    def showlinearreg(self):
+        Showliner()
+    
+    def showpolyreg(self):
+        Showpoly()
+
+    def prediction(self):
+        global insert
+        
+        tk.messagebox.showinfo("Prediction","The Predicted Salary is "+str(PredictionModel([[insert]])))
+        
+        
+        
+
+
+if __name__ == '__main__':    
+
+    value=g()
+    value.mainloop()
+    
+
+    
---
+++ SalaryPrediction/SalaryPredictionModel.py
@@ -0,0 +1,135 @@
+# Polynomial regression
+# importing libraries
+
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+
+def showdataset():
+    dataset= pd.read_csv('Position_Salaries.csv')
+
+    return(dataset)
+    
+def Showliner():
+     #importing dataset 
+
+    dataset= pd.read_csv('Position_Salaries.csv')
+    X = dataset.iloc[:, 1:2].values
+
+    y = dataset.iloc[:, 2].values
+
+    # Fitting Simple Linear Regression to the dataset
+
+    from sklearn.linear_model import LinearRegression
+
+    lin_reg = LinearRegression()
+    lin_reg.fit(X, y)  
+
+
+    #Visualising  the Linear Regression result
+    plt.scatter(X,y,color='red')
+    plt.plot(X,lin_reg.predict(X),color='blue')
+    plt.title('Level V/s Salary (Linear Regression)')
+    plt.xlabel('Level')
+    plt.ylabel('Salary')
+    plt.show()
+
+def Showpoly():
+    #importing dataset 
+
+    dataset= pd.read_csv('Position_Salaries.csv')
+
+    
+
+    X = dataset.iloc[:, 1:2].values
+
+    y = dataset.iloc[:, 2].values
+
+    # Fitting Simple Linear Regression to the dataset
+
+    from sklearn.linear_model import LinearRegression
+
+    lin_reg = LinearRegression()
+    lin_reg.fit(X, y)  
+
+    # Fitting Polynomial Regression to the dataset
+
+    from sklearn.preprocessing import PolynomialFeatures
+
+    poly_reg = PolynomialFeatures(degree=4)
+    X_poly = poly_reg.fit_transform(X)
+    lin_reg2 = LinearRegression()
+    lin_reg2.fit(X_poly,y)
+
+    
+
+    #Visualising  the Polynomial Regression result
+    X_grid = np.arange(min (X),max (X),0.1)
+    X_grid = X_grid.reshape((len(X_grid),1))
+    
+    plt.scatter(X, y, color='red')
+    plt.plot(X_grid,lin_reg2.predict(poly_reg.fit_transform(X_grid)),color = 'blue')
+    plt.title('Regression')
+    plt.xlabel('Level')
+    plt.ylabel('Salary')
+    plt.show()
+
+
+    
+
+
+    
+    
+
+
+def PredictionModel(mat):
+    #importing dataset 
+
+    dataset= pd.read_csv('Position_Salaries.csv')
+
+    
+
+    X = dataset.iloc[:, 1:2].values
+
+    y = dataset.iloc[:, 2].values
+
+    # Fitting Simple Linear Regression to the dataset
+
+    from sklearn.linear_model import LinearRegression
+
+    lin_reg = LinearRegression()
+    lin_reg.fit(X, y)  
+
+    # Fitting Polynomial Regression to the dataset
+
+    from sklearn.preprocessing import PolynomialFeatures
+
+    poly_reg = PolynomialFeatures(degree=4)
+    X_poly = poly_reg.fit_transform(X)
+    lin_reg2 = LinearRegression()
+    lin_reg2.fit(X_poly,y)
+
+    #Visualising  the Linear Regression result
+    plt.scatter(X,y,color='red')
+    plt.plot(X,lin_reg.predict(X),color='blue')
+    plt.title('Level V/s Salary (Linear Regression)')
+    plt.xlabel('Level')
+    plt.ylabel('Salary')
+    #plt.show()
+
+
+    #Visualising  the Polynomial Regression result
+    X_grid = np.arange(min (X),max (X),0.1)
+    X_grid = X_grid.reshape((len(X_grid),1))
+    
+    plt.scatter(X, y, color='red')
+    plt.plot(X_grid,lin_reg2.predict(poly_reg.fit_transform(X_grid)),color = 'blue')
+    plt.title('Regression')
+    plt.xlabel('Level')
+    plt.ylabel('Salary')
+    #plt.show()
+
+    
+    #Predicting a new result with Linear Regression model
+    return(int(lin_reg.predict(mat)))
+
